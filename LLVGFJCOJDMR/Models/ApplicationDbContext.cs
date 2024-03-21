b7b2b32ede6c5dc2bdede747baf91b0f0296c6fa
Juan David Molina Ruano <mr24004@esfe.agape.edu.sv>
2024-03-21 09:08:30 -0600

    add entity customer and phone numbrea to application dbcontext

---
+++ LLVGFJCOJDMR/Models/ApplicationDbContext.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace LLVGFJCOJDMR.Models
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<PhoneNumber> PhoneNumbers { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Customer>()
+                .HasMany(p => p.PhoneNumbers)
+                .WithOne(d => d.Customer)
+                .OnDelete(DeleteBehavior.Cascade);
+
+            //insertar datos inicales a la tabla rols
+            //modelBuilder.Entity<Rol>().HasData(new Rol { Id = 1, Name = "Admin", Description = "soy admin" });
+
+            //modelBuilder.Entity<User>().HasData(new User { Id = 1, Username = "Root", RolId = 1, Password = "e10adc3949ba59abbe56e057f20f883e" });
+        }
+
+    }
+}
