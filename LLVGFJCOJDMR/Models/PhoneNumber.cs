cdbdfe839ef49b88e9de5f26b76c41acba91658a
Juan David Molina Ruano <mr24004@esfe.agape.edu.sv>
2024-03-21 16:47:49 -0600

    modify data anotation stringleght

--- LLVGFJCOJDMR/Models/PhoneNumber.cs
+++ LLVGFJCOJDMR/Models/PhoneNumber.cs
@@ -13,12 +13,12 @@ namespace LLVGFJCOJDMR.Models
 
         //--------------
         [Required(ErrorMessage = "El campo {0} es requerido")]
-        [MaxLength(20, ErrorMessage = "El campo {0} no puede tener mas de {1} caracteres")]
+        [StringLength(20, ErrorMessage = "El campo {0} no puede tener mas de {1} caracteres")]
         [RegularExpression("^[0-9]*$", ErrorMessage = "El campo {0} solo puede contener números")]
         public string NumberPhone { get; set; }
 
         [Required(ErrorMessage = "El campo {0} es requerido")]
-        [MaxLength(100, ErrorMessage = "El campo {0} no puede tener mas de {1} caracteres")]
+        [StringLength(100, ErrorMessage = "El campo {0} no puede tener mas de {1} caracteres")]
         [RegularExpression("^[a-zA-Z ]*$", ErrorMessage = "El campo {0} solo puede contener letras")]
         public string Note { get; set; }
 
