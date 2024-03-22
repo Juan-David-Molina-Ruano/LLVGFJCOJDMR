24ebdfe1bf3865e25e4ca46097d6d1c0b6c5f123
Lorena Lisseth Vasquez Godoy <2431186@clases.edu.sv>
2024-03-21 21:21:22 -0600

    were added the display in Users

--- LLVGFJCOJDMR/Models/User.cs
+++ LLVGFJCOJDMR/Models/User.cs
@@ -9,15 +9,19 @@ namespace LLVGFJCOJDMR.Models
         public byte[]? Image { get; set; }
         [Key]
         public int Id { get; set; }
+        [Display(Name = "Rol")]
         public int RolId { get; set; }
         [Required(ErrorMessage = "El campo Nombre es obligatorio.")]
+        [Display(Name ="Nombre Usuario")]
         public string UserName { get; set; }
         [Required(ErrorMessage = "El campo Contraseña es obligatorio.")]
+        [Display(Name ="Contraseña")]
         public string Password { get; set; }
         [Required(ErrorMessage = "El campo Email es obligatorio.")]
         [EmailAddress(ErrorMessage = "El campo Email debe ser una dirección de correo electrónico válida.")]
         public string Email { get; set; }
         [Required(ErrorMessage = "El campo Estado es obligatorio.")]
+        [Display(Name ="Estado")]
         public int Status { get; set; }
 
         public virtual Rol Rol { get; set; }
