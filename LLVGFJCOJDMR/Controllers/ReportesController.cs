cdde7948e77f5053e974d3b1509f95a33dadd48e
Lorena Lisseth Vasquez Godoy <2431186@clases.edu.sv>
2024-03-21 20:32:38 -0600

    fix reportscontroller

--- LLVGFJCOJDMR/Controllers/ReportesController.cs
+++ LLVGFJCOJDMR/Controllers/ReportesController.cs
@@ -5,6 +5,7 @@ using Microsoft.EntityFrameworkCore;
 using Rotativa.AspNetCore;
 using OfficeOpenXml;
 using OfficeOpenXml.Style;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
 
 namespace LLVGFJCOJDMR.Controllers
 {
@@ -21,13 +22,13 @@ namespace LLVGFJCOJDMR.Controllers
         {
             return View();
         }
-
+        [HttpPost]
         public async Task<IActionResult> GenerarReporte(string firstName, string lastName, string opcion)
         {
             var customers = await _context.Customers.Include(c => c.PhoneNumbers).Where(c => c.FirstName == firstName).ToListAsync();
             if (customers.Count == 0)
             {
-                ViewBag.ErrorMessage = "No se encontraron ventas para el cliente proporcionado.";
+                ViewBag.ErrorMessage = "No se encontraron clientes.";
                 return View("Index");
             }
             else 
@@ -46,60 +47,45 @@ namespace LLVGFJCOJDMR.Controllers
                         worksheet.Column(3).Width = 30; // Establecer el ancho de la columna C en 30
                         worksheet.Column(4).Width = 10; // Establecer el ancho de la columna D en 30
                         worksheet.Column(5).Width = 25; // Establecer el ancho de la columna E en 30
-                        worksheet.Column(6).Width = 15; // Establecer el ancho de la columna F en 30
-                        worksheet.Column(7).Width = 19; // Establecer el ancho de la columna G en 30
-                        worksheet.Column(8).Width = 8; // Establecer el ancho de la columna H en 30
-                        worksheet.Column(9).Width = 14; // Establecer el ancho de la columna I en 30
+                        
 
-                        worksheet.Cells["A1"].Value = "Cliente";
-                        worksheet.Cells["B1"].Value = "Empleado";
-                        worksheet.Cells["C1"].Value = "Detalles";
-                        worksheet.Cells["D1"].Value = "Color";
-                        worksheet.Cells["E1"].Value = "Detalles de estilo";
-                        worksheet.Cells["F1"].Value = "Marca";
-                        worksheet.Cells["G1"].Value = "Zapato";
-                        worksheet.Cells["H1"].Value = "Talla";
-                        worksheet.Cells["I1"].Value = "Fecha de venta";
+                        worksheet.Cells["A1"].Value = "Nombre";
+                        worksheet.Cells["B1"].Value = "Apellido";
+                        worksheet.Cells["C1"].Value = "Email";
+                        
+                
 
                         int fila = 2;
-                        foreach (var item in ventas)
+                        foreach (var item in customers)
                         {
                             int columna = 1;
-                            worksheet.Cells[fila, columna].Value = item.Cliente.Nombre;
+                            worksheet.Cells[fila, columna].Value = item.FirstName;
 
                             columna = 2;
-                            worksheet.Cells[fila, columna].Value = item.Empleado.Nombre;
+                            worksheet.Cells[fila, columna].Value = item.LastName;
 
-                            foreach (var item1 in item.DetalleVentas)
+                            columna = 3;
+                            worksheet.Cells[fila, columna].Value = item.Email;
+
+                            foreach (var item1 in item.PhoneNumbers)
                             {
-                                columna = 3;
-                                worksheet.Cells[fila, columna].Value = item1.Zapato.Descripcion;
+                              
 
                                 columna = 4;
-                                worksheet.Cells[fila, columna].Value = item1.Zapato.DetalleEstilo.Color;
+                                worksheet.Cells[fila, columna].Value = item1.NumberPhone;
 
                                 columna = 5;
-                                worksheet.Cells[fila, columna].Value = item1.Zapato.DetalleEstilo.Nombre;
+                                worksheet.Cells[fila, columna].Value = item1.Note;
 
-                                columna = 6;
-                                worksheet.Cells[fila, columna].Value = item1.Zapato.DetalleEstilo.Marca.Nombre;
-
-                                columna = 7;
-                                worksheet.Cells[fila, columna].Value = item1.Zapato.Descripcion;
-
-                                columna = 8;
-                                worksheet.Cells[fila, columna].Value = item1.Zapato.Talla.Nombre;
+                                
                             }
 
-                            columna = 9;
-                            worksheet.Cells[fila, columna].Style.Numberformat.Format = "yyyy-MM-dd";
-                            worksheet.Cells[fila, columna].Value = item.FechaVenta;
                             fila++;
 
                             int lastRow = worksheet.Dimension.End.Row;
 
                             // Obtener el rango de celdas que deseas aplicar el borde
-                            ExcelRange range1 = worksheet.Cells[1, 1, lastRow, 9];
+                            ExcelRange range1 = worksheet.Cells[1, 1, lastRow, 6];
 
                             // Aplicar bordes
                             range1.Style.Border.Top.Style = ExcelBorderStyle.Thin;
@@ -114,15 +100,17 @@ namespace LLVGFJCOJDMR.Controllers
 
                         byte[]? filecontents = package.GetAsByteArray();
 
-                        return File(filecontents, "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet", "Reporte de Venta.xlsx");
+                        return File(filecontents, "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet", "Reporte de Clientes.xlsx");
 
                     }
+                    
                 }
-                  
-            }
-           
-           
+                else
+                {
+                    return Content("Opcion no valida");
+                }
 
+            }
         }
 
     }
